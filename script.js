bc56d1daf28c792b4e05a097cf1b614b03cfbee3
Vijay vemuri <vijayvemuri225@gmail.com>
2024-01-13 04:08:23 +0000

    deploy

---
+++ script.js
@@ -0,0 +1,110 @@
+//turn pages when click on next or prev button
+const pageTurnBtn=document.querySelectorAll('.nextprev-btn');
+
+pageTurnBtn.forEach((el,index)=>{
+    el.onclick=()=>{
+        const pageTurnId=el.getAttribute('data-page');
+        const pageTurn=document.getElementById(pageTurnId);
+
+        if(pageTurn.classList.contains('turn')){
+            pageTurn.classList.remove('turn');
+            setTimeout(()=>{
+                pageTurn.style.zIndex =20 - index;
+            },500)
+        }
+        else{
+            pageTurn.classList.add('turn');
+            setTimeout(()=>{
+                pageTurn.style.zIndex =20 + index;
+            },500)
+        }
+    }
+})
+
+//Contract me
+
+const pages =document.querySelectorAll('.book-page.page-right');
+const contractMeBtn = document.querySelector('.btn.contact-me');
+
+contractMeBtn.onclick = ()=>{
+    pages.forEach((page,index)=>{
+        setTimeout(()=>{
+            page.classList.add('turn');
+        setTimeout(()=>{
+            page.style.zIndex = 20 +index;
+        },500)
+        },(index + 1) * 200 + 100)
+    })
+
+}
+
+
+//reverse index
+
+let totalPages = pages.length;
+let pageNumber=0;
+
+function reverseIndex(){
+    pageNumber--;
+    if(pageNumber<0){
+        pageNumber = totalPages -1;
+
+    }
+}
+
+//back profile
+
+const backProfileBtn=document.querySelector('.back-profile');
+
+backProfileBtn.onclick=()=>{
+    pages.forEach((_,index)=>{
+        setTimeout(()=>{
+            reverseIndex();
+            pages[pageNumber].classList.remove('turn');
+
+            setTimeout(()=>{
+                pages[pageNumber].style.zIndex =10 + index;
+            },500)
+        },(index + 1)* 200 + 100)
+
+    })
+
+}
+
+
+//opening
+
+const coverRight = document.querySelector('.cover.cover-right');
+const pageLeft = document.querySelector('.book-page.page-left');
+
+
+//opening Animation
+
+setTimeout(()=>{
+    coverRight.classList.add('turn')
+},2100)
+
+setTimeout(()=>{
+    coverRight.style.zIndex = -1;
+
+},2800)
+// page left and right
+setTimeout(()=>{
+    pageLeft.style.zIndex = 20;
+
+},3200)
+
+//opening all pages
+
+pages.forEach((_,index)=>{
+    setTimeout(()=>{
+        reverseIndex();
+        pages[pageNumber].classList.remove('turn');
+
+        setTimeout(()=>{
+            pages[pageNumber].style.zIndex =10 + index;
+        },500)
+    },(index + 1)* 200 + 2100)
+
+})
+
